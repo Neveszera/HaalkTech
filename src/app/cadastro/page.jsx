22bfd93483bd7c715e565b761b46c2c882760d25
Neveszera <gabrielgng26@gmail.com>
2023-11-24 22:13:09 -0300

    Cadastro funcionando com Json.

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,18 +1,164 @@
 "use client"
 
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/Cadastro.module.css';
 
 const Cadastro = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    nome: '',
+    cpf: '',
+    dataNascimento: '',
+    telefoneCelular: '',
+    endereco: '',
+    sexo: '',
+    email: '',
+    confirmarEmail: '',
+    senha: '',
+    confirmarSenha: '',
+    autorizacao: false,
+  });
+  const [users, setUsers] = useState([]);
+  const [mensagem, setMensagem] = useState('');
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const loadedUsers = localStorage.getItem('db.json');
+      if (loadedUsers) {
+        setUsers(JSON.parse(loadedUsers).users || []);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleTogglePassword = () => {
     setShowPassword((prev) => !prev);
   };
 
-  const handleCadastro = () => {
-    console.log('Cadastro realizado!');
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const validateInputs = () => {
+    if (
+      !formData.nome ||
+      !formData.cpf ||
+      !formData.dataNascimento ||
+      !formData.telefoneCelular ||
+      !formData.endereco ||
+      !formData.sexo ||
+      !formData.email ||
+      !formData.confirmarEmail ||
+      !formData.senha ||
+      !formData.confirmarSenha
+    ) {
+      setMensagem('Todos os campos são obrigatórios.');
+      return false;
+    }
+
+    if (formData.email !== formData.confirmarEmail) {
+      setMensagem('Os emails não coincidem.');
+      return false;
+    }
+
+    if (formData.senha !== formData.confirmarSenha) {
+      setMensagem('As senhas não coincidem.');
+      return false;
+    }
+
+    return true;
+  };
+
+  const isDuplicateUser = () => {
+    return users.some(
+      (user) =>
+        user.email.toLowerCase() === formData.email.toLowerCase() ||
+        user.cpf === formData.cpf
+    );
+  };
+
+  const handleCadastro = async () => {
+    if (!validateInputs()) {
+      return;
+    }
+
+    const newUser = { ...formData, id: users.length + 1 };
+
+    if (isDuplicateUser()) {
+      setMensagem('Já existe um usuário cadastrado com o mesmo email ou CPF.');
+      setIsError(true);
+      setIsSuccess(false);
+      return;
+    }
+
+    const updatedUsers = [...users, newUser];
+
+    try {
+      const response = await fetch('http://localhost:8080/GlobalSolution/rest/paciente', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+
+      if (!response.ok) {
+        console.error('Erro ao cadastrar usuário na API:', response.statusText);
+        await sendToServerJson(newUser); // Se falhar, tenta no servidor JSON
+      } else {
+        console.log('Cadastro realizado com sucesso na API!');
+        setMensagem('Cadastro realizado com sucesso!');
+        setIsSuccess(true);
+        setIsError(false);
+      }
+    } catch (error) {
+      console.error('Erro ao conectar com a API:', error);
+      await sendToServerJson(newUser); // Se falhar, tenta no servidor JSON
+    }
+
+    setUsers(updatedUsers);
+  };
+
+  const sendToServerJson = async (data) => {
+    try {
+      const response = await fetch('http://localhost:5000/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        console.error('Erro ao cadastrar usuário no servidor JSON:', response.statusText);
+        setMensagem('Erro ao cadastrar usuário.');
+        setIsError(true);
+        setIsSuccess(false);
+      } else {
+        console.log('Cadastro realizado com sucesso no servidor JSON!');
+        setMensagem('Cadastro realizado com sucesso!');
+        setIsSuccess(true);
+        setIsError(false);
+      }
+    } catch (error) {
+      console.error('Erro ao conectar com o servidor JSON:', error);
+      setMensagem('Erro ao conectar com o servidor.');
+      setIsError(true);
+      setIsSuccess(false);
+    }
+  };
+
+  const handleDismiss = () => {
+    setIsSuccess(false);
+    setIsError(false);
   };
 
   return (
@@ -25,16 +171,40 @@ const Cadastro = () => {
       <div className={styles.cadastroRight}>
         <div className={styles.cadastroForm}>
           <h1>Cadastro</h1>
-          <input type="text" placeholder="Nome" className={styles.input} />
-          <input type="text" placeholder="CPF" className={styles.input} />
-          <input type="text" placeholder="Data de Nascimento" className={styles.input} />
-          <input type="text" placeholder="Telefone Celular" className={styles.input} />
-          <input type="text" placeholder="Endereço" className={styles.input} />
-          <input type="text" placeholder="Sexo" className={styles.input} />
-          <input type="email" placeholder="Email" className={styles.input} />
-          <input type="email" placeholder="Confirmar Email" className={styles.input} />
+          <input type="text" placeholder="Nome" className={styles.input} onChange={handleInputChange} name="nome" />
+          <input type="text" placeholder="CPF" className={styles.input} onChange={handleInputChange} name="cpf" />
+          <input
+            type="text"
+            placeholder="Data de Nascimento"
+            className={styles.input}
+            onChange={handleInputChange}
+            name="dataNascimento"
+          />
+          <input
+            type="text"
+            placeholder="Telefone Celular"
+            className={styles.input}
+            onChange={handleInputChange}
+            name="telefoneCelular"
+          />
+          <input type="text" placeholder="Endereço" className={styles.input} onChange={handleInputChange} name="endereco" />
+          <input type="text" placeholder="Sexo" className={styles.input} onChange={handleInputChange} name="sexo" />
+          <input type="email" placeholder="Email" className={styles.input} onChange={handleInputChange} name="email" />
+          <input
+            type="email"
+            placeholder="Confirmar Email"
+            className={styles.input}
+            onChange={handleInputChange}
+            name="confirmarEmail"
+          />
           <div className={styles.passwordInput}>
-            <input type={showPassword ? 'text' : 'password'} placeholder="Senha" className={styles.input} />
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Senha"
+              className={styles.input}
+              onChange={handleInputChange}
+              name="senha"
+            />
             <button type="button" onClick={handleTogglePassword} className={styles.togglePasswordButton}>
               <Image
                 src={`/img/icons/olho${showPassword ? '-aberto' : '-fechado'}.svg`}
@@ -45,7 +215,13 @@ const Cadastro = () => {
             </button>
           </div>
           <div className={styles.passwordInput}>
-            <input type={showPassword ? 'text' : 'password'} placeholder="Confirmar Senha" className={styles.input} />
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder="Confirmar Senha"
+              className={styles.input}
+              onChange={handleInputChange}
+              name="confirmarSenha"
+            />
             <button type="button" onClick={handleTogglePassword} className={styles.togglePasswordButton}>
               <Image
                 src={`/img/icons/olho${showPassword ? '-aberto' : '-fechado'}.svg`}
@@ -56,9 +232,16 @@ const Cadastro = () => {
             </button>
           </div>
           <div className={styles.checkboxContainer}>
-            <input type="checkbox" className={styles.checkbox} />
-            <label className={styles.checkboxLabel}>Eu autorizo a utilização dos meus dados e respostas para fins de treinamento de inteligência artificial.</label>
+            <input type="checkbox" className={styles.checkbox} onChange={handleInputChange} name="autorizacao" />
+            <label className={styles.checkboxLabel}>
+              Eu autorizo a utilização dos meus dados e respostas para fins de treinamento de inteligência artificial.
+            </label>
           </div>
+          {mensagem && (
+            <div className={isSuccess ? styles.successMessage : styles.errorMessage} onClick={handleDismiss}>
+              {mensagem}
+            </div>
+          )}
           <button className={styles.cadastrarButton} onClick={handleCadastro}>
             Cadastrar
           </button>
---
+++ src/app/usuario/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Usuario = ({ userData }) => {
+  return (
+    <section>
+      <div>
+        <h1>Bem-vindo, Gabriel!</h1>
+      </div>
+      <div >
+        {/* Conteúdo da página do usuário */}
+      </div>
+    </section>
+  );
+};
+
+export default Usuario;
